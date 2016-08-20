36d698eb4b2c37a1c67d4731261d85ab912b4014
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-08-21 01:49:05 +0300

    signIn added

--- src/db.js
+++ src/db.js
@@ -1,7 +1,5 @@
 'use strict';
-
 const firebase = require("firebase");
-
 const firebaseConfig = {
   apiKey: "AIzaSyD1YNK7qmU03pn6SOMC9_GnTG1eB8-A-gs",
   authDomain: "healther-31b19.firebaseapp.com",
@@ -9,6 +7,57 @@ const firebaseConfig = {
   storageBucket: "healther-31b19.appspot.com",
 };
 
-firebase.initializeApp(firebaseConfig);
+export default class {
+  constructor() {
+    firebase.initializeApp(firebaseConfig);
 
-export const dbRef = firebase.database().ref();
+
+  }
+
+  get ref() {
+    return firebase.database().ref();
+  }
+
+  signIn() {
+    firebase.auth().signInWithRedirect(new firebase.auth.FacebookAuthProvider())
+      .then(function (result) {
+        if (result.credential) {
+          // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+          var token = result.credential.accessToken;
+          // ...
+        }
+        // The signed-in user info.
+        var user = result.user;
+      })
+      .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+    /*firebase.auth().signInWithPopup(new firebase.auth.FacebookAuthProvider())
+     .then(function (result) {
+     // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+     var token = result.credential.accessToken;
+     // The signed-in user info.
+     var user = result.user;
+     // ...
+     console.log('login', result)
+     })
+     .catch(function (error) {
+     // Handle Errors here.
+     var errorCode = error.code;
+     var errorMessage = error.message;
+     // The email of the user's account used.
+     var email = error.email;
+     // The firebase.auth.AuthCredential type that was used.
+     var credential = error.credential;
+     // ...
+     });*/
+  }
+}
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 'use strict';
 require('./index.scss');
 
-const dbRef = require('./db').dbRef;
+const DbConstructor = require('./db').default;
+const db = new DbConstructor();
 
 const header = document.querySelector('.header');
 
-dbRef.child('slogan').on('value', snap => header.innerText = snap.val());
+const signInBtn = document.createElement('input');
+signInBtn.setAttribute('type', 'button');
+signInBtn.setAttribute('value', 'signIn');
+signInBtn.addEventListener('click', ()=>db.signIn());
+header.appendChild(signInBtn);
+
+//.child('slogan').on('value', snap => header.innerText = snap.val());
