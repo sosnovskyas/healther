5e6f942f68fa237810ebd0241516ae4b4e6f4323
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-08-20 12:50:01 +0300

    webpack settings

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const autoprefixer = require('autoprefixer');
 const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
 module.exports = {
-  devtool: isDevelopment ? 'cheap-module-inline-source-map' : null,
+  devtool: isDevelopment ? 'source-map' : null,
 
   entry: [
     './src/index.js'
